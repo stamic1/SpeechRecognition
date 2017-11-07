ba955edf36b6ab0a0bc74099e65ea8f48c61d4e9
Gaurav Bhat <gauravbhat@Gauravs-MacBook-Air.local>
2017-11-07 11:51:35 +0530

    Initial Commit

---
+++ SpeechRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognition
+//
+//  Created by Gaurav Bhat on 07/11/17.
+//  Copyright © 2017 Gaurav Bhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
